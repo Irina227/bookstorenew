d82622794e0ffe1612d1b3539117aa2b886a295d
Irina227 <47809810+Irina227@users.noreply.github.com>
2022-05-16 23:01:10 +0300

    Add files via upload

---
+++ bookstorenew/js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-left',
+    prevEl: '.swiper-button-right',
+  },
+});
+
